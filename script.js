de9efe91ceb2dd80c17df70f6955fbb2c7e92528
Grt3d <hatembrahmy1@gmail.com>
2025-04-08 04:28:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,64 @@
-(function() {
-    const TOKEN = "e2219079c5ae4e04befb7d426d5804d0";  // التوكن المُحدث
-    const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36";  // وكيل المستخدم المُحدث
+(function () {
+    const TOKEN = "e2219079c5ae4e04befb7d426d5804d0";
+    const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36";
 
-    // دالة لإرسال POST
     function sendPost(url, data) {
         return fetch(url, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json, text/plain, */*",
-                "User-Agent": USER_AGENT,
                 "Origin": "https://kitty-web.bfp72q.com",
                 "Referer": "https://kitty-web.bfp72q.com/"
+                // ملاحظة: User-Agent لا يمكن تعيينه من داخل المتصفح
             },
             body: JSON.stringify(data)
         }).then(response => response.json())
           .catch(error => console.error("Error sending request:", error));
     }
 
-    // دالة لاستخراج UIDs
-    function extractAllUids(response) {
+    function extractAllUids(responseObj) {
         const uids = [];
-        const uidKeyword = "\"uid\":";
-        let index = 0;
-        while ((index = response.indexOf(uidKeyword, index)) !== -1) {
-            const startIndex = response.indexOf(":", index) + 2;
-            const endIndex = response.indexOf("\"", startIndex);
-            const uid = response.substring(startIndex, endIndex);
-            uids.push(uid);
-            index = endIndex;
+        function recursiveSearch(data) {
+            if (Array.isArray(data)) {
+                data.forEach(item => recursiveSearch(item));
+            } else if (typeof data === 'object' && data !== null) {
+                for (const key in data) {
+                    if (key === 'uid') {
+                        uids.push(data[key]);
+                    } else {
+                        recursiveSearch(data[key]);
+                    }
+                }
+            }
         }
+        recursiveSearch(responseObj);
         return uids;
     }
 
-    // دالة لجلب بيانات المشهد وإرسال الطلبات
-    function collectEggRewards() {
-        const infoJson = { "token": TOKEN };
-        sendPost("https://kitty-api.bfp72q.com/api/scene/info", infoJson)
-            .then(response => {
-                if (response) {
-                    const uids = extractAllUids(JSON.stringify(response));
-                    uids.forEach(uid => {
-                        const rewardJson = { "token": TOKEN, "egg_uid": uid };
-                        sendPost("https://kitty-api.bfp72q.com/api/scene/egg/reward", rewardJson)
-                            .then(() => {
-                                console.log("Reward sent for UID: " + uid);
-                            });
-                    });
-                }
-            });
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    // تنفيذ الدالة فور دخول الزائر إلى الموقع
-    collectEggRewards();
+    async function collectEggRewards() {
+        const infoJson = { token: TOKEN };
+        const response = await sendPost("https://kitty-api.bfp72q.com/api/scene/info", infoJson);
 
+        if (response) {
+            const uids = extractAllUids(response);
+            console.log(`Found ${uids.length} UIDs.`);
+
+            for (const uid of uids) {
+                const rewardJson = { token: TOKEN, egg_uid: uid };
+                const res = await sendPost("https://kitty-api.bfp72q.com/api/scene/egg/reward", rewardJson);
+                console.log("Reward sent for UID: " + uid, res);
+                await sleep(300); // تأخير بسيط لتجنب حظر السيرفر
+            }
+        } else {
+            console.warn("No response received from scene/info");
+        }
+    }
+
+    // تشغيل الدالة تلقائيًا
+    collectEggRewards();
 })();
